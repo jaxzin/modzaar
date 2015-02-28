06ca6dc91ab52b36c2e209bbc0b092b6bbc906df
Brian R. Jackson <brian@jaxzin.com>
2015-02-28 15:22:18 -0500

    Refactored example Mod into proper package/classname.

--- src/main/java/com/jaxzin/modzaar/Modzaar.java
+++ src/main/java/com/jaxzin/modzaar/Modzaar.java
@@ -1,12 +1,12 @@
-package com.example.examplemod;
+package com.jaxzin.modzaar;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = Modzaar.MODID, version = Modzaar.VERSION)
+public class Modzaar
 {
     public static final String MODID = "modzaar";
     public static final String VERSION = "1.0-SNAPSHOT";
