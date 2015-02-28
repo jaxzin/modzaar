d10e0ac9ca6b065783fd33328d37ff2855664759
Brian R. Jackson <brian@jaxzin.com>
2015-02-28 14:40:15 -0500

    Switched mod information to this project.

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -8,8 +8,8 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "modzaar";
+    public static final String VERSION = "1.0-SNAPSHOT";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
